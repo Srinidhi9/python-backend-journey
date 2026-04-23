3e91388c93e3ec12052a19e10e27f2c9358006de
sree <srinidhireddy9948@gmail.com>
2026-04-23 11:17:23 +0530

    Day 6 SQL practice added

---
+++ tracked/day06/April_practice.sql
@@ -0,0 +1,55 @@
+-- CREATE DATABASE student_db; 
+-- USE student_db;
+-- CREATE TABLE students (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(50),
+--     age INT,
+--     course VARCHAR(50)
+-- );
+-- INSERT INTO students (name, age, course)
+-- VALUES ('Alice', 22, 'CS');
+-- INSERT INTO students (name, age, course)
+-- VALUES
+-- ('Bob', 20, 'IT'),
+-- ('Charlie', 23, 'CS'),
+-- ('David', 21, 'ECE'),
+-- ('Emma', 24, 'Mechanical'),
+-- ('Frank', 19, 'CS');
+-- SELECT * FROM students; 
+-- SELECT * FROM students WHERE age > 20;
+-- SELECT * FROM students ORDER BY age DESC;
+-- SELECT * FROM students ORDER BY age ASC;
+-- SELECT * FROM students LIMIT 2;
+-- SELECT * FROM users WHERE username='admin';
+
+-- INSERT INTO students (name, age, course)
+-- VALUES
+-- ('sree', 20, 'CS'),
+-- ('Nidhi', 21, 'IT'),
+-- ('Reddy', 22, 'DS'),
+-- ('vamshi', 23, 'CS'),
+-- ('dubba', 19, 'AIML'),
+-- ('Vasu', 24, 'CS'),
+-- ('suchi', 23, 'CS'),
+-- ('pooja', 25, 'IT'),
+-- ('prasu', 26, 'ECE'),
+-- ('kavya' , 21, 'EEE');
+
+-- SELECT * from students where age > 21;
+-- select * from students where course = 'CS';
+-- select * from students ORDER BY name;
+-- select * from students ORDER BY age ASC LIMIT 3;
+ create table course(
+ id SERIAL PRIMARY KEY,
+ course_name VARCHAR(50)
+ );
+ 
+
+
+
+
+
+
+
+
+
