26910f35f2e4dbf296b6b94b6d90b413d68a14de
sree <srinidhireddy9948@gmail.com>
2026-04-30 21:48:47 +0530

    Day 13-14: Django setup, models, templates, and full UI CRUD implementation

---
+++ myproject/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home),
+    path('add/', views.add_post),
+    path('delete/<int:id>/', views.delete_post),
+    path('edit/<int:id>/', views.edit_post),
+]
---
+++ myproject/blog/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Post
+
+
+def home(request):
+    posts = Post.objects.all()
+    return render(request, "blog/home.html", {"posts": posts})
+
+
+def add_post(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+
+        if title and content:
+            Post.objects.create(title=title, content=content)
+
+    return redirect("/")
+
+
+def delete_post(request, id):
+    post = get_object_or_404(Post, id=id)
+    post.delete()
+    return redirect("/")
+
+
+def edit_post(request, id):
+    post = get_object_or_404(Post, id=id)
+
+    if request.method == "POST":
+        post.title = request.POST.get("title")
+        post.content = request.POST.get("content")
+        post.save()
+        return redirect("/")
+
+    return render(request, "blog/edit.html", {"post": post})
