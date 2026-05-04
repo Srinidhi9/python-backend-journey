0f52bdcf110f2d06633e73f0d8a87f577e4353f5
sree <srinidhireddy9948@gmail.com>
2026-05-04 13:15:36 +0530

    Added authentication system (login/logout + protection)

--- myproject/blog/urls.py
+++ myproject/blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('add/', views.add_post),
     path('delete/<int:id>/', views.delete_post),
     path('edit/<int:id>/', views.edit_post),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
 ]
--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 
 @login_required
 def home(request):
@@ -35,3 +36,22 @@ def edit_post(request, id):
         return redirect("/")
 
     return render(request, "blog/edit.html", {"post": post})
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("/")
+        else:
+            return render(request, "blog/login.html", {"error": "Invalid credentials"})
+
+    return render(request, "blog/login.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect("/login/")
