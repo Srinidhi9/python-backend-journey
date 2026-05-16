29c28ea8050e2a17d645d81cc1ca8a4ccfc0aa6b
sree <srinidhireddy9948@gmail.com>
2026-05-16 16:03:42 +0530

    Added JWT authentication with FastAPI

---
+++ fastapi-day1/app/auth.py
@@ -0,0 +1,22 @@
+from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta
+
+SECRET_KEY = "mysecretkey"
+
+pwd_context = CryptContext(schemes=["bcrypt"])
+
+def hash_password(password):
+    return pwd_context.hash(password)
+
+def verify_password(plain, hashed):
+    return pwd_context.verify(plain, hashed)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(minutes=30)
+
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(to_encode, SECRET_KEY, algorithm="HS256")
