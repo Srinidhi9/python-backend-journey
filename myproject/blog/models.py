d90af8db5905751755da4b04d31983242c2a1323
sree <srinidhireddy9948@gmail.com>
2026-05-16 13:26:21 +0530

    Day 5: Added models and Django admin integration

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -6,3 +6,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
