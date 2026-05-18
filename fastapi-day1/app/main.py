518d858c59900745dc820999f20d0fa8bf6ed8bf
sree <srinidhireddy9948@gmail.com>
2026-05-18 17:11:39 +0530

    Temporary Swagger debug

--- fastapi-day1/app/main.py
+++ fastapi-day1/app/main.py
@@ -1,55 +1,6 @@
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy.orm import Session
-
-from app.database import SessionLocal, engine, Base
-from app.models import User
-from app.schemas import UserCreate, Login
-from app.auth import hash_password, verify_password, create_access_token
-
-Base.metadata.create_all(bind=engine)
+from fastapi import FastAPI
 
 app = FastAPI()
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@app.post("/signup")
-def signup(user: UserCreate, db: Session = Depends(get_db)):
-
-    hashed_pw = hash_password(user.password)
-
-    new_user = User(
-        username=user.username,
-        email=user.email,
-        password=hashed_pw
-    )
-
-    db.add(new_user)
-    db.commit()
-
-    return {"message": "User created"}
-
-
-@app.post("/login")
-def login(user: Login, db: Session = Depends(get_db)):
-
-    db_user = db.query(User).filter(User.username == user.username).first()
-
-    if not db_user:
-        raise HTTPException(status_code=400, detail="Invalid username")
-
-    if not verify_password(user.password, db_user.password):
-        raise HTTPException(status_code=400, detail="Wrong password")
-
-    token = create_access_token({"sub": db_user.username})
-
-    return {"access_token": token}
 
 @app.get("/")
 def home():
